1c2690b9629852a28d6a47b0482d80c0a16d2e86
Abhishek Tiwari <abhie.lp@gmail.com>
2021-11-09 00:37:26 +0530

    Create the main function to run the server

---
+++ app/__init__.py
@@ -0,0 +1,21 @@
+"""Main file to run the app"""
+
+from flask import Flask
+from flask_sock import Sock
+
+sock = Sock()
+
+
+def create_app() -> Flask:
+    """Create flask app using the factory pattern"""
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY="hands_on_flask-sock_package",
+    )
+    
+    @app.route("/test")
+    def test():
+        return "Flask server running."
+
+    sock.init_app(app)
+    return app
